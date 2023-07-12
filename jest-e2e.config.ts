de751c4fc3b5eba3416f2de68c4bd659ba40cbee
Mnigos <makowskiigor@gmail.com>
2023-07-12 12:32:42 +0200

    chore(jest.config): fix configuration

--- jest-e2e.config.ts
+++ jest-e2e.config.ts
@@ -2,23 +2,10 @@ import type { Config } from 'jest'
 
 import defaultJestConfig from './jest.config'
 
-const {
-  moduleFileExtensions,
-  rootDir,
-  transform,
-  moduleNameMapper,
-  testEnvironment,
-  testPathIgnorePatterns,
-} = defaultJestConfig
-
 const configuration: Config = {
-  moduleFileExtensions,
-  rootDir,
-  testEnvironment,
-  transform,
-  moduleNameMapper,
-  testPathIgnorePatterns,
-  testRegex: '.e2e-spec.ts$',
+  ...defaultJestConfig,
+  testMatch: ['<rootDir>/test/**/*.e2e-spec.ts'],
+  roots: ['<rootDir>/test'],
 }
 
 export default configuration
--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,7 @@ import type { Config } from 'jest'
 const configuration: Config = {
   moduleFileExtensions: ['js', 'json', 'ts'],
   rootDir: '.',
-  testRegex: '.*\\.spec\\.ts$',
+  testMatch: ['<rootDir>/src/**/*.spec.ts'],
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest',
   },
@@ -13,22 +13,11 @@ const configuration: Config = {
     '<rootDir>/node_modules/',
     '<rootDir>/dist/',
     '<rootDir>/.stryker-tmp/',
-    '.js',
   ],
   moduleNameMapper: {
     '^~/(.*)$': '<rootDir>/src/$1',
-    '^@modules/app(.*)$': '<rootDir>/src/modules/app/$1',
-    '^@modules/auth(.*)$': '<rootDir>/src/modules/auth/$1',
-    '^@modules/users(.*)$': '<rootDir>/src/modules/users/$1',
-    '^@modules/messages(.*)$': '<rootDir>/src/modules/messages/$1',
-    '^@modules/embeddings(.*)$': '<rootDir>/src/modules/embeddings/$1',
-    '^@modules/openai(.*)$': '<rootDir>/src/modules/openai/$1',
-    '^@modules/sessions(.*)$': '<rootDir>/src/modules/sessions/$1',
-    '^@common/filters(.*)$': '<rootDir>/src/common/filters/$1',
-    '^@common/enums(.*)$': '<rootDir>/src/common/enums/$1',
-    '^@common/constants(.*)$': '<rootDir>/src/common/constants/$1',
-    '^@common/decorators(.*)$': '<rootDir>/src/common/decorators/$1',
-    '^@common/api-messages(.*)$': '<rootDir>/src/common/api-messages',
+    '^@modules/app(.*)$': '<rootDir>/src/modules/$1',
+    '^@common/filters(.*)$': '<rootDir>/src/common/$1',
   },
   coveragePathIgnorePatterns: [
     '.*\\.(interface|module|schema|entity|dto|enum|d).ts',
