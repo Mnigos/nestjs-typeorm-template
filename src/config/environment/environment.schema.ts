e97668325d0cb286ee8ce96706f88624d23ed498
Mnigos <makowskiigor@gmail.com>
2023-10-19 15:51:59 +0200

    chore(environment): add database variables

--- src/config/environment/environment.schema.ts
+++ src/config/environment/environment.schema.ts
@@ -2,6 +2,16 @@ import * as Joi from 'joi'
 
 /* eslint-disable import/namespace */
 export const environmentValidationSchema = Joi.object({
-  PORT: Joi.number().default(4000),
+  NODE_ENV: Joi.string()
+    .valid('development', 'production', 'test', 'provision')
+    .default('development'),
+
+  PORT: Joi.number().empty('').default(4000),
+
+  DATABASE_HOST: Joi.string().required(),
+  DATABASE_PORT: Joi.number().required(),
+  DATABASE_USERNAME: Joi.string().required(),
+  DATABASE_PASSWORD: Joi.string().required(),
+  DATABASE_NAME: Joi.string().required(),
 })
 /* eslint-enable import/namespace */
